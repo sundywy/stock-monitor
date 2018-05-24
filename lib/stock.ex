3c1de82e299ac4febea1c787e60b04e71d125f38
Sundy <sundy@call-levels.com>
2018-05-24 16:41:46 +0800

    stock: Using agent instead of genserver

--- lib/stock.ex
+++ lib/stock.ex
@@ -1,24 +1,25 @@
 defmodule StockMonitor.Stock do
-  use GenServer
-
+  use Agent
+  
   alias __MODULE__
 
   defmodule StockData do
     defstruct [:ticker_symbol, :trade_type, :price, :quantity, :id]
   end
   
-  def start_link({_, _, _, _, _} = stock_data) do
-    GenServer.start_link(__MODULE__, :ok, stock_data)
+  def start_link({ticker_symbol, trade_type, price, quantity, id} = stock_data) do
+    stock = %StockData{ticker_symbol: ticker_symbol, trade_type: trade_type, price: price, quantity: quantity, id: id}
+    # GenServer.start_link(__MODULE__, [stock])
+
+    Agent.start_link(fn -> stock end)
   end
 
-  def stop(pid) do
-    GenServer.call(pid, :stop)
+  def get(pid) do
+    Agent.get(pid, fn x -> x end)
   end
   
-  ## callback
-  
-  def init({ticker_symbol, trade_type, price, quantity, id}) do
-    {:ok, %StockData{ticker_symbol: ticker_symbol, trade_type: trade_type, price: price, quantity: quantity, id: id}}
+  def stop(pid) do
+    Agent.stop(pid)
   end
 
 end
