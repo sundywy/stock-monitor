43bc6eeec579be9840c5c64759ab10e2e44f5a69
Sundy <sundy@call-levels.com>
2018-05-25 10:47:53 +0800

    stock_supervisor: converted to dynamic supervisor

--- lib/stock_supervisor.ex
+++ lib/stock_supervisor.ex
@@ -1,21 +1,24 @@
 defmodule StockMonitor.StockSupervisor do
-  use Supervisor
+  use DynamicSupervisor
 
+  alias StockMonitor.Stock
+  
   def start_link(arg) do
     Supervisor.start_link(__MODULE__, arg)
   end
 
   def init(_arg) do
-
-    children = [
-      StockMonitor.Stock,
-    ]
-
-    Supervisor.init(children, strategy: :one_for_one)
+    DynamicSupervisor.init(strategy: :one_for_one)
   end
   
-  def start_child(pid, {_, _, _, _, _} = state) do
-    Supervisor.start_child(pid, state)
+  def start_child({_, _, _, _, _} = state) do
+
+    child_spec = {Stock, start: {Stock, :start_link, [state]}}
+    DynamicSupervisor.start_child(__MODULE__, child_spec)
   end
 
+  def get_pid() do
+    {:ok, self()}
+  end
+  
 end
